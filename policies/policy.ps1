233cb16ffac00a3543b024f621cdf4b91417f818
marcel.nguyen <marcel.nguyen@outlook.de>
2025-04-08 21:48:23 +0200

    update

--- policies/policy.ps1
+++ policies/policy.ps1
@@ -1,28 +1,16 @@
 $params = @{
-	displayName = "Access to EXO requires MFA"
+	displayName = "Require MFA to EXO from non-compliant devices."
 	state = "enabled"
 	conditions = @{
-		clientAppTypes = @(
-		"mobileAppsAndDesktopClients"
-	"browser"
-)
-applications = @{
-	includeApplications = @(
-	"00000002-0000-0ff1-ce00-000000000000"
-)
-}
-users = @{
-includeGroups = @(
-"ba8e7ded-8b0f-4836-ba06-8ff1ecc5c8ba"
-)
-}
-locations = @{
-includeLocations = @(
-"All"
-)
-excludeLocations = @(
-"AllTrusted"
-)
+		applications = @{
+			includeApplications = @(
+			"00000002-0000-0ff1-ce00-000000000000"
+		)
+	}
+	users = @{
+		includeGroups = @(
+		"ba8e7ded-8b0f-4836-ba06-8ff1ecc5c8ba"
+	)
 }
 }
 grantControls = @{
